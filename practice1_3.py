b568067acb8a62ece85c263ef094a1f581403f96
0Astronaut0 <amir.hosseinask84@gmail.com>
2024-03-03 23:24:01 +0330

    Create practice1_3.py

---
+++ practice1_3.py
@@ -0,0 +1,13 @@
+#amirhossein askari
+#40213160281828
+operator = input("select operator (sum,difference,multiple,divide): ")
+num1 = int(input("entre num1: "))
+num2 = int(input("entre num2: "))
+if(operator == "sum"):
+    print(num1 + num2)
+elif(operator == "difference"):
+    print(num1 - num2)
+elif(operator == "multiple"):
+    print(num1 * num2)
+elif(operator == "divide"):
+    print(num1 / num2)
